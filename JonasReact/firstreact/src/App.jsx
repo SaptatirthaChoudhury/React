bf33c0956e70ffa84ef2493e923d133a71e914ba
SaptatirthaChoudhury <saptatirtha@gmail.com>
2025-02-06 17:17:34 +0530

    Stop tracking package-lock.json

--- JonasReact/firstreact/src/App.jsx
+++ JonasReact/firstreact/src/App.jsx
@@ -1,34 +1,31 @@
-
-import { useEffect, useState } from 'react';
-import './App.css'
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import "./App.css";
 
 export default function App() {
-  const [advice, setAdvice] = useState('');
+  const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
-  
 
   async function getAdvice() {
-    const res = await fetch('https://api.adviceslip.com/advice');
+    const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
-    setCount(count + 1)
+    setCount(count + 1);
   }
 
-  useEffect(
-    function () {
-      getAdvice();
-    },
-    []
-  )
+  useEffect(function () {
+    getAdvice();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="App">
       <h1>Hello Jonas</h1>
       <h2>{advice}</h2>
       <button onClick={getAdvice}>Get advice</button>
-      <Message count = {count}/>
+      <Message count={count} />
     </div>
-  )
+  );
 }
 
 function Message(props) {
@@ -36,6 +33,10 @@ function Message(props) {
     <p>
       You have read <strong>{props.count}</strong> pieces of advice
     </p>
-  )
+  );
 }
 
+// Add prop-types validation
+Message.propTypes = {
+  count: PropTypes.number.isRequired,
+};
